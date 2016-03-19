157ca0995026d333686ae353e4c989f65b140a3f
Nick Geneva <ngeneva@udel.edu>
2016-03-18 23:02:21 -0400

    Leaflet Map Plug In
    -Added leaf let map plug in for a pan-able and zoomable map veiw
    -Created map tiles of ori and the blind forest via python script from:
    http://build-failed.blogspot.com/2012/11/zoomable-image-with-leaflet.html

--- js/mapPage.js
+++ js/mapPage.js
@@ -3,6 +3,15 @@
  */
 $(document).ready(function() {
 
+    var map = L.map('map').setView([-45, 30], 4);
+    L.tileLayer('/Ori/img/Maps/FullMap/{z}/{x}/{y}.png', {
+        minZoom: 0,
+        maxZoom: 5,
+        attribution: 'Full Map',
+        tms: true
+    }).addTo(map);
+
+
     $(".mapContainer").mousemove(function(e){
         var bgImage = new Image();
         bgImage.src = $(this).css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
